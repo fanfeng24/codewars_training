4fbcfee1420e0bbc0ede8bc0b4d5cc6b46e8f573
fanfeng24 <fan.feng24@gmail.com>
2017-06-15 23:58:41 -0700

    Update solution.cpp

--- Find the stray number/solution.cpp
+++ Find the stray number/solution.cpp
@@ -2,16 +2,18 @@ int stray(std::vector<int> numbers) {
     int i1 = numbers[0];
     int i2 = numbers[1];
     int j = 1;
+    
+    if(i1 != i2){
+        if (i1 = numbers[2])
+        return i2;
+        else
+        return i1;
+    }
     while (i1 == i2){
         if (numbers[j]!=numbers[j+1])
         {
         return numbers[j+1];
         break;}
-    }
-    j=0;
-    while (i1 != i2){
-        if (numbers[0] = numbers[j+2])
-        return i2;
-        else
-        return i1;
-    break;}
+        j++;
+    }  
+};
